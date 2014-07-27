d16e9d6976f6d4447338dd292288cf26faf447b1
Steven Chien <steven.c.chien@gmail.com>
2014-07-27 14:31:01 -0700

    Initial Commit

---
+++ CommentThreadTree/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CommentThreadTree
+//
+//  Created by Steven Chien on 7/27/14.
+//  Copyright (c) 2014 stevenchien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CommentThreadTree/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CommentThreadTree
+//
+//  Created by Steven Chien on 7/27/14.
+//  Copyright (c) 2014 stevenchien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
